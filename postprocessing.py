4eb257434a3ccdbcb1c5488b667d765c1e116cea
SparkliTwizzl (they/them) <github@sparklitwizzl.com>
2023-05-10 12:57:24 -0600

    reduce max items to 20

--- postprocessing.py
+++ postprocessing.py
@@ -12,7 +12,7 @@ from tiktokapipy.async_api import AsyncTikTokAPI
 # Custom Domain
 githubPagesUrl = "https://tiktokrss.uselesslesbians.gay/"
 
-maxItems = 30;
+maxItems = 20;
 subscriptionFileCount = 5
 
 
